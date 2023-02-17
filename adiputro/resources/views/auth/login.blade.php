8c4208aa911b9bf7f1a5804f3371b6b5ec48bd0a
Angelizines <angelita_j20@mhs.istts.ac.id>
2023-02-17 20:20:38 +0700

    login UI

---
+++ adiputro/resources/views/auth/login.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <section class="h-full gradient-form bg-slate-300 md:h-screen">
+        <div class="container py-12 px-6 h-full">
+          <div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+            <div class="xl:w-10/12">
+              <div class="block bg-white shadow-lg rounded-lg">
+                <div class="lg:flex lg:flex-wrap g-0">
+                  <div class="lg:w-6/12 px-4 md:px-0">
+                    <div class="md:p-12 md:mx-6">
+                      <div class="text-center">
+                        <img
+                          class="mx-auto w-48"
+                          src={{ asset('img/adiputro_logo.svg') }}
+                          alt="logo"
+                        />
+                        {{-- <h1 class="text-lg font-semibold mt-1 mb-12 pb-1">ADIPUTRO</h1> --}}
+                      </div>
+                      <form>
+                        <p class="my-6">Please login to your account</p>
+                        <div class="mb-4">
+                          <input
+                            type="text"
+                            class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            id="exampleFormControlInput1"
+                            placeholder="Username"
+                          />
+                        </div>
+                        <div class="mb-4">
+                          <input
+                            type="password"
+                            class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            id="exampleFormControlInput1"
+                            placeholder="Password"
+                          />
+                        </div>
+                        <div class="text-center pt-1 mb-12 pb-1">
+                          <button
+                            class="inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-slate-200 hover:text-black hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3 bg-gray-800"
+                            type="button"
+                            data-mdb-ripple="true"
+                            data-mdb-ripple-color="light"
+                            style=""
+                          >
+                            Log in
+                          </button>
+                          {{-- <a class="text-gray-500" href="#!">Forgot password?</a> --}}
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                  <div
+                    class="lg:w-6/12 flex items-center lg:rounded-r-lg rounded-b-lg lg:rounded-bl-none bg-[url('/img/bus_wall_1.jpg')] bg-cover"
+                  >
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+</body>
+</html>
