6780649253b3985628a549b2d471506e36c35b86
Jonathan Theja <105841081+JonathanTheja@users.noreply.github.com>
2023-02-17 18:24:33 +0700

    coba sidebar

---
+++ adiputro/resources/views/sidebar.blade.php
@@ -0,0 +1,10 @@
+<div class="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-300px overflow-y-auto text-center bg-gray-900">
+    <div class="text-gray-100 text-xl">
+        <div class="p-2.5 mt-1 flex items-center">
+            <i class="fas fa-search"></i>
+            <h1 class="font-bold text-gray-200 text-[15px] ml-3">
+                Tailwind
+            </h1>
+        </div>
+    </div>
+</div>
