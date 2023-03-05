559db5e116316216fe2eedf95cca4089d9a07a47
Angelizines <angelita_j20@mhs.istts.ac.id>
2023-03-05 13:08:19 +0700

    Update data_edit.blade.php

--- adiputro/resources/views/master/partials/data_edit.blade.php
+++ adiputro/resources/views/master/partials/data_edit.blade.php
@@ -47,6 +47,37 @@
                     <option value="{{ $pe->process_entry_id }}">{{$pe->work_description}}</option>
                 @endforeach
             </select>
+            <div class="w-9/12 rounded-lg py-5">
+                <h1 class="text-lg" id="component_name">Tabel Process Entry</h1>
+                <div id="table_container">
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                        <table class="w-full text-sm text-left text-gray-500">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        No
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Kode Komponen
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Nama Komponen
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        QTY
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody id="tableCol">
+                                
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+
+                <div id="photosLoader"></div>
+            </div>
             <button type="submit"
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-5">Update Komponen</button>
 
