83d06a7a0864b82b2d000dc87fd16a8acaace9da
Angelizines <angelita_j20@mhs.istts.ac.id>
2023-03-07 11:44:29 +0700

    Update data_edit.blade.php

--- adiputro/resources/views/master/partials/data_edit.blade.php
+++ adiputro/resources/views/master/partials/data_edit.blade.php
@@ -115,19 +115,19 @@
     function updateEntryTable(){
         let selected_processes = $("#input-process").val();
         // $("#process_entries_container").html("");
-        $("#input-process option:selected").each(function () {
-            //generate table
-            let body_name = "process_entry_body_"+$(this).val();
+        let input_selected = $("#input-process option:selected").last();
+        let body_name = "process_entry_body_"+input_selected.val();
+        //generate table
             $("#process_entries_container").append(
                 `<div class="w-9/12 rounded-lg py-5">
-                    <h1 class="text-lg my-3">Tabel Process Entry `+$(this).text()+`</h1>
+                    <h1 class="text-lg my-3">Tabel Process Entry `+input_selected.text()+`</h1>
                     <div>
                         <div class="flex flex-wrap -mx-3 mb-2">
                             <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="component_input_`+$(this).val()+`" type="text" placeholder="Kode Komponen">
+                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="component_input_`+input_selected.val()+`" type="text" placeholder="Kode Komponen">
                             </div>
                             <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none" onclick="addRowTable('`+body_name+`','`+$(this).val()+`')">+</button>
+                                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none" onclick="addRowTable('`+body_name+`','`+input_selected.val()+`')">+</button>
                             </div>
                         </div>
 
@@ -149,22 +149,14 @@
                                         </th>
                                     </tr>
                                 </thead>
-                                <tbody id="process_entry_body_`+$(this).val()+`">
+                                <tbody id="process_entry_body_`+input_selected.val()+`">
                                 </tbody>
                             </table>
                         </div>
                     </div>
                 </div>`
             );
-
-            // var $this = $(this);
-            // if ($this.length) {
-            //     var selText = $this.text();
-            //     console.log(selText);
-            // }
-        });
     }
-
     function addRowTable(id,table_number){
         let component_code = $("#component_input_"+table_number).val();
 
