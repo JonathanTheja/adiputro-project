c3112c3390dc44df4bfbd97a19a68a929a3397e0
Angelizines <angelita_j20@mhs.istts.ac.id>
2023-03-25 09:24:54 +0700

    hm

--- adiputro/app/Http/Controllers/ProcessEntryController.php
+++ adiputro/app/Http/Controllers/ProcessEntryController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProcessEntry;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProcessEntryController extends Controller
 {
@@ -10,6 +12,12 @@ class ProcessEntryController extends Controller
     function index()
     {
         # code...
-        return view('master.process_entry');
+        //get all process entries
+        $pes = ProcessEntry::all();
+        $department = Auth::user()->department->name;
+        return view('master.process_entry',[
+            "process_entries"=>$pes,
+            "department"=>$department
+        ]);
     }
 }
--- adiputro/resources/views/master/process_entry.blade.php
+++ adiputro/resources/views/master/process_entry.blade.php
@@ -25,24 +25,23 @@
                   </tr>
                 </thead>
                 <tbody>
-                  <tr class="border-b dark:border-neutral-500">
-                    <td class="px-6 py-4 font-medium">1</td>
-                    <td class="px-6 py-4">Mark</td>
-                    <td class="px-6 py-4">Otto</td>
-                    <td class="px-6 py-4">@mdo</td>
+                @foreach ($process_entries as $process_entry)
+                 <tr class="border-b dark:border-neutral-500">
+                    <td class="px-6 py-4 font-medium">{{ $loop->iteration }}</td>
+                    <td class="px-6 py-4">{{ $process_entry->process_number }}</td>
+                    <td class="px-6 py-4">{{ $process_entry->stall_number }}</td>
+                    <td class="px-6 py-4">{{ $process_entry->work_description }}</td>
+                    <td class="px-6 py-4">{{ $process_entry->pic}}</td>
+                    <td class="px-6 py-4">{{ $process_entry->spk_type}}</td>
+                    <td class="px-6 py-4">{{ $department}}</td>
+                    <td class="px-6 py-4">{{ $process_entry->spk_type}}</td>
+                    <td class="px-6 py-4">{{ $process_entry->spk_type}}</td>
+                    <td class="px-6 py-4">{{ $process_entry->spk_type}}</td>
+                    <td class="px-6 py-4">
+                        <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Hapus</button>
+                    </td>
                   </tr>
-                  <tr class="border-b dark:border-neutral-500">
-                    <td class="px-6 py-4 font-medium">2</td>
-                    <td class="px-6 py-4">Jacob</td>
-                    <td class="px-6 py-4">Thornton</td>
-                    <td class="px-6 py-4">@fat</td>
-                  </tr>
-                  <tr class="border-b dark:border-neutral-500">
-                    <td class="px-6 py-4 font-medium">3</td>
-                    <td class="px-6 py-4">Larry</td>
-                    <td class="px-6 py-4">Wild</td>
-                    <td class="px-6 py-4">@twitter</td>
-                  </tr>
+                @endforeach
                 </tbody>
               </table>
             </div>
