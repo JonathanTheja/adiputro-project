9969d3c0404ba6c676ca88d281f0cdd423e81ef6
Angelizines <angelita_j20@mhs.istts.ac.id>
2023-07-01 09:10:38 +0700

    Update dashboard.blade.php

--- adiputro/resources/views/master/dashboard.blade.php
+++ adiputro/resources/views/master/dashboard.blade.php
@@ -53,25 +53,7 @@
                 <span class="text-gray-500">Dashboard</span>
             </div>
             <div id="tracker-item" class="flex items-center">
-                <div class="flex items-center ml-4">
-                    <div class="mr-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
-                            <path
-                                d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
-                        </svg>
-                    </div>
-                    <span class="text-gray-500">Bus</span>
-                </div>
 
-                <div class="flex items-center ml-4">
-                    <div class="mr-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
-                            <path
-                                d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
-                        </svg>
-                    </div>
-                    <span class="text-gray-500">SDD</span>
-                </div>
             </div>
         </div>
         <hr class="border-2 border-gray-400">
